8fca4a393d1bb274331247a5628c0997a2e760e3
usuckxyz <87349369+usuckxyz@users.noreply.github.com>
2021-11-21 12:03:32 -0300

    Delete keysystem.lua

--- keysystem.lua
+++
@@ -1,7 +0,0 @@
-local key = _G.Key
-local check = "https://raw.githubusercontent.com/usuckxyz/fluidxyz/main/check.php?key=" .. key
-if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/usuckxyz/fluidxyz/main/fluidxyz.lua"))()
-else
-game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
-end
