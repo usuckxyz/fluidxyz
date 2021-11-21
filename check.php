81d7a70318fd3254b503ac95b865e4458a849d66
usuckxyz <87349369+usuckxyz@users.noreply.github.com>
2021-11-21 11:22:25 -0300

    Update check.php

--- check.php
+++ check.php
@@ -15,8 +15,9 @@ $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 $fh = fopen('logs.txt', 'a'); 
 fwrite($fh, ''."".$ip ."\n");
 $keys = array(
-"kahdsakdh2alsdh11",
+"Key",
 
+"kirbo",   
 ); 
 $sub = $_GET["key"];
 if (in_array($sub,$keys,TRUE)) {
